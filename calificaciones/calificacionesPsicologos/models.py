0c7a37df8e5a14c2b35d26713d84a4b53e3a5d11
Juan <j.lora@uniandes.edu.co>
2023-11-05 15:09:49 -0500

    sonar

--- calificaciones/calificacionesPsicologos/models.py
+++ calificaciones/calificacionesPsicologos/models.py
@@ -29,6 +29,12 @@ class Calificaciones(models.Model):
     mensaje = models.CharField(max_length=200)
     puntuacion = models.FloatField()
 
+
+class Calificacionessonar(models.Model):
+    psicologo = models.ForeignKey(Psicologo, on_delete=models.CASCADE)
+    mensaje = models.CharField(max_length=200)
+    puntuacion = models.FloatField()
+
 class Psicologo245(models.Model):
     nombre = models.CharField(max_length=100) 
     promedio_calificaciones = models.FloatField()
